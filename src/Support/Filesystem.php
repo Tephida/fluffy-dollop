e9fc5ceb335c9bbb169a8383f1425c2f721f8d8b
Semen Alekseev <semyon492@ya.ru>
2022-03-04 12:07:13 +0300

    update

--- src/Support/Filesystem.php
+++ src/Support/Filesystem.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * Copyright (c) 2022. Semen Alekseev
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
+ *
+ */
 
 namespace FluffyDollop\Support;
 
