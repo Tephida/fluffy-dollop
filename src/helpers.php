68c4541ad79c44755d13d1c48cdb20b70803309a
Semen Alekseev <semyon492@ya.ru>
2022-04-09 21:12:26 +0300

    fix

--- src/helpers.php
+++ src/helpers.php
@@ -136,9 +136,50 @@ function to_translit(string $value, bool $lower = true, bool $part = true): arra
  * @param $stamp
  * @return string
  */
-function langdate($format, $stamp): string
+function langdate(string $format, $stamp): string
 {
-    global $langdate;
+    $langdate = array(
+        'January' => "января",
+        'February' => "февраля",
+        'March' => "марта",
+        'April' => "апреля",
+        'May' => "мая",
+        'June' => "июня",
+        'July' => "июля",
+        'August' => "августа",
+        'September' => "сентября",
+        'October' => "октября",
+        'November' => "ноября",
+        'December' => "декабря",
+        'Jan' => "янв",
+        'Feb' => "фев",
+        'Mar' => "мар",
+        'Apr' => "апр",
+        'Jun' => "июн",
+        'Jul' => "июл",
+        'Aug' => "авг",
+        'Sep' => "сен",
+        'Oct' => "окт",
+        'Nov' => "ноя",
+        'Dec' => "дек",
+
+        'Sunday' => "Воскресенье",
+        'Monday' => "Понедельник",
+        'Tuesday' => "Вторник",
+        'Wednesday' => "Среда",
+        'Thursday' => "Четверг",
+        'Friday' => "Пятница",
+        'Saturday' => "Суббота",
+
+        'Sun' => "Вс",
+        'Mon' => "Пн",
+        'Tue' => "Вт",
+        'Wed' => "Ср",
+        'Thu' => "Чт",
+        'Fri' => "Пт",
+        'Sat' => "Сб",
+    );
+
     return strtr(date($format, (int)$stamp), $langdate);
 }
 
